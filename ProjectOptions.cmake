d52c0a1b3b892eccf2efa2a5cc7cd8e79577779a
Template Janitor <template.janitor@example.com>
2026-03-11 07:34:47 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(LearnOpenGL_supports_sanitizers)
   # Emscripten doesn't support sanitizers
   if(EMSCRIPTEN)
     set(SUPPORTS_UBSAN OFF)
@@ -58,110 +58,110 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(LearnOpenGL_setup_options)
+  option(LearnOpenGL_ENABLE_HARDENING "Enable hardening" ON)
+  option(LearnOpenGL_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    LearnOpenGL_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    LearnOpenGL_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  LearnOpenGL_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR LearnOpenGL_PACKAGING_MAINTAINER_MODE)
+    option(LearnOpenGL_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(LearnOpenGL_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(LearnOpenGL_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(LearnOpenGL_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(LearnOpenGL_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(LearnOpenGL_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(LearnOpenGL_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(LearnOpenGL_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(LearnOpenGL_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(LearnOpenGL_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(LearnOpenGL_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(LearnOpenGL_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(LearnOpenGL_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(LearnOpenGL_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(LearnOpenGL_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(LearnOpenGL_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(LearnOpenGL_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(LearnOpenGL_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(LearnOpenGL_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(LearnOpenGL_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(LearnOpenGL_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(LearnOpenGL_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(LearnOpenGL_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(LearnOpenGL_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      LearnOpenGL_ENABLE_IPO
+      LearnOpenGL_WARNINGS_AS_ERRORS
+      LearnOpenGL_ENABLE_SANITIZER_ADDRESS
+      LearnOpenGL_ENABLE_SANITIZER_LEAK
+      LearnOpenGL_ENABLE_SANITIZER_UNDEFINED
+      LearnOpenGL_ENABLE_SANITIZER_THREAD
+      LearnOpenGL_ENABLE_SANITIZER_MEMORY
+      LearnOpenGL_ENABLE_UNITY_BUILD
+      LearnOpenGL_ENABLE_CLANG_TIDY
+      LearnOpenGL_ENABLE_CPPCHECK
+      LearnOpenGL_ENABLE_COVERAGE
+      LearnOpenGL_ENABLE_PCH
+      LearnOpenGL_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  LearnOpenGL_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (LearnOpenGL_ENABLE_SANITIZER_ADDRESS OR LearnOpenGL_ENABLE_SANITIZER_THREAD OR LearnOpenGL_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(LearnOpenGL_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(LearnOpenGL_global_options)
+  if(LearnOpenGL_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    LearnOpenGL_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  LearnOpenGL_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(LearnOpenGL_ENABLE_HARDENING AND LearnOpenGL_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR LearnOpenGL_ENABLE_SANITIZER_UNDEFINED
+       OR LearnOpenGL_ENABLE_SANITIZER_ADDRESS
+       OR LearnOpenGL_ENABLE_SANITIZER_THREAD
+       OR LearnOpenGL_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${LearnOpenGL_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${LearnOpenGL_ENABLE_SANITIZER_UNDEFINED}")
+    LearnOpenGL_enable_hardening(LearnOpenGL_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(LearnOpenGL_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(LearnOpenGL_warnings INTERFACE)
+  add_library(LearnOpenGL_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  LearnOpenGL_set_project_warnings(
+    LearnOpenGL_warnings
+    ${LearnOpenGL_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
@@ -172,66 +172,66 @@ macro(myproject_local_options)
 
   if(NOT EMSCRIPTEN)
     include(cmake/Sanitizers.cmake)
-    myproject_enable_sanitizers(
-      myproject_options
-      ${myproject_ENABLE_SANITIZER_ADDRESS}
-      ${myproject_ENABLE_SANITIZER_LEAK}
-      ${myproject_ENABLE_SANITIZER_UNDEFINED}
-      ${myproject_ENABLE_SANITIZER_THREAD}
-      ${myproject_ENABLE_SANITIZER_MEMORY})
+    LearnOpenGL_enable_sanitizers(
+      LearnOpenGL_options
+      ${LearnOpenGL_ENABLE_SANITIZER_ADDRESS}
+      ${LearnOpenGL_ENABLE_SANITIZER_LEAK}
+      ${LearnOpenGL_ENABLE_SANITIZER_UNDEFINED}
+      ${LearnOpenGL_ENABLE_SANITIZER_THREAD}
+      ${LearnOpenGL_ENABLE_SANITIZER_MEMORY})
   endif()
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(LearnOpenGL_options PROPERTIES UNITY_BUILD ${LearnOpenGL_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(LearnOpenGL_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      LearnOpenGL_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(LearnOpenGL_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    LearnOpenGL_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(LearnOpenGL_ENABLE_CLANG_TIDY)
+    LearnOpenGL_enable_clang_tidy(LearnOpenGL_options ${LearnOpenGL_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(LearnOpenGL_ENABLE_CPPCHECK)
+    LearnOpenGL_enable_cppcheck(${LearnOpenGL_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(LearnOpenGL_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    LearnOpenGL_enable_coverage(LearnOpenGL_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(LearnOpenGL_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(LearnOpenGL_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(LearnOpenGL_ENABLE_HARDENING AND NOT LearnOpenGL_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR LearnOpenGL_ENABLE_SANITIZER_UNDEFINED
+       OR LearnOpenGL_ENABLE_SANITIZER_ADDRESS
+       OR LearnOpenGL_ENABLE_SANITIZER_THREAD
+       OR LearnOpenGL_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    LearnOpenGL_enable_hardening(LearnOpenGL_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
